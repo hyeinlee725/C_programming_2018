c413c45f5a2b02868a020b75d8669d5a2c499e9f
Lee Hye-In <59090549+hyeinlee725@users.noreply.github.com>
2022-03-31 14:48:00 +0900

    Add files via upload

---
+++ struct/struct.cpp
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include <string.h>
+
+struct point
+{
+	int xpos;
+	int ypos;
+};
+
+struct person
+{
+	char name[20];//name 자체가 주소
+	char phoneNum[20];
+	int age;
+};
+int main()
+{
+	struct point pos = { 10, 20 };
+	struct person man = { "이승기", "010-1212-0001", 21 };
+
+	printf("%p %p\n", &pos, &pos.xpos);
+	printf("%p %p\n", &man, man.name);//name 자체가 주소기 때문에 주소 표시 않붙이는 것이 맞음!
+	return 0;
+}
