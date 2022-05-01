0d59bcbc75b768d12138d182b2bad8bf00058e4e
Lee Hye-In <59090549+hyeinlee725@users.noreply.github.com>
2022-05-01 12:47:37 +0900

    Add files via upload

--- ReadUserName/sou.cpp
+++ ReadUserName/sou.cpp
@@ -1,23 +1,26 @@
 #pragma warning(disabled:1996)
 #include <stdio.h>
+#include <stdlib.h>
 
-char name[30];
-
-char* ReadUserName()
+char* ReadUserName(void)
 {
-	printf("What's your name? ");
-	gets_s(name);
+	char* name = (char*)malloc(sizeof(char) * 30);
+	printf("What's your name?");
+	gets_s(name, 30);
 	return name;
 }
+
 int main()
 {
 	char* name1;
 	char* name2;
 	name1 = ReadUserName();
-	printf("name1: %s \n", name1);
-	name2 = ReadUserName();//결국 name1과 name2가 같은 곳을 가리키기 때문에 name1도 name2의 이름으로 나타남
-	printf("name2: %s \n", name2);
-	printf("name1: %s \n", name1);
-	printf("name2: %s \n", name2);
+	printf("name1: %s\n", name1);
+	name2 = ReadUserName();
+	printf("name2: %s\n", name2);
+	printf("again name1: %s\n", name1);
+	printf("again name2: %s\n", name2);
+	free(name1);
+	free(name2);
 	return 0;
 }
